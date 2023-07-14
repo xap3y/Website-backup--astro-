990cad124242c002080d53c18f439d0f5d35a494
XAP3X_ <xap3xdev@gmail.com>
2023-07-14 11:42:12 +0200

    updates

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,26 @@ export const site = {
     header: "XAP3X_",
     description: "I'm XAP3X_ :>"
 }
+
+export const socials = [
+    {
+      name: "GitHub",
+      link: "https://github.com/XAP3xOnTop",
+      icon: "github.svg",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/xap.3x/",
+      icon: "instagram.svg",
+    },
+    {
+      name: "Spigot MC",
+      link: "https://www.spigotmc.org/members/minecubek.797215/",
+      icon: "spigotmc.png",
+    },
+  ] satisfies {
+    name: string;
+    link: string;
+    icon: string;
+  }[];
   
