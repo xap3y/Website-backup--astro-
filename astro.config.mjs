c1ec99cf40e16cb12ef10cd28e57584072d96d54
Sinista121 <115770708+Sinista121@users.noreply.github.com>
2023-10-14 20:44:18 +0000

    About update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    vite: {
-        resolve: {
-          alias: {
-            "~": resolve(__dirname, "./src"),
-            "@layouts": resolve(__dirname, "./src/layouts"),
-            "@components": resolve(__dirname, "./src/components"),
-          },
-        },
+  integrations: [tailwind()],
+  server: {
+    port: 3000,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "~": resolve(__dirname, "./src"),
+        "@layouts": resolve(__dirname, "./src/layouts"),
+        "@components": resolve(__dirname, "./src/components"),
       },
+    },
+  },
 });
